acaadf219d84f5aed11990a52f56d653f929b575
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-26 22:11:51 +0530

    create onMarkerClick and onClose methods

--- googlemap-with-react/src/App.js
+++ googlemap-with-react/src/App.js
@@ -7,6 +7,29 @@ const mapStyles = {
 };
 
 class MapContainer extends React.Component {
+
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
       <div className="App">
