e04a044f82ee35ab24cc9bf810206266a17bf7e5
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-26 09:42:38 +0530

    use mapbox instead google map to react and do simple implementation in react

--- googlemap-with-react/src/App.js
+++ googlemap-with-react/src/App.js
@@ -1,42 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+// ES6
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
-
-const style = {
-  width: '50%',
-  height: '50%'
-}
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiZGlsei1tYXBib3giLCJhIjoiY2thbmVkYW9zMDJ1bzJ1cW4wOGMzaTBmbCJ9.fFVVCkpjc6-R1i_8jPmB3g'
+});
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-        <Map google={this.props.google} zoom={14} initialCenter={{ lat: 6.927079, lng: 79.861244 }}
-        // containerStyle={containerStyle}
-        style={style}
-        centerAroundCurrentLocation={false}
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: '100vh',
+            width: '100vw'
+          }}
         >
-
-          <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
-
-          <InfoWindow onClose={this.onInfoWindowClose}>
-
-          </InfoWindow>
+          <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+          </Layer>
         </Map>
       </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBZMsaoXAt7JqaEV9suMQR_wVCt3QLCMoc")
-})(App)
+export default App
