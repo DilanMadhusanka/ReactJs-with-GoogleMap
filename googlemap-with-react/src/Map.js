821e578f76e880da3acddfea6a6669b13b51e46a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-26 22:35:43 +0530

    create a new file and name it Map.js and create a component named CurrentLocation

---
+++ googlemap-with-react/src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CurrentLocation extends React.Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default CurrentLocation;
